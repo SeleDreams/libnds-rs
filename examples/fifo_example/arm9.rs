3e23498e7068da6f2f2c2184c1404fbcc36298ed
SeleDreams <seledreams.contact@gmail.com>
2023-02-13 21:32:29 +0100

    Fixed debug build crashing, lto is required

--- examples/fifo_example/arm9.rs
+++ examples/fifo_example/arm9.rs
@@ -4,6 +4,7 @@ extern crate alloc;
 #[start]
 fn main(_argc: isize, _argv: *const *const u8) -> isize 
 {
+    let mut frame = 0;
     unsafe
     {
         consoleDemoInit();
@@ -15,14 +16,13 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize
             let mut values : [u32;3] = [0;3];
             let mut ctr = 0;
             while ctr < 3 {
-                libc_println!("Hello World");
                 if fifoCheckValue32(FIFO_USER_01 as i32)
                 {
                     values[ctr] = fifoGetValue32(FIFO_USER_01 as i32);
                     ctr += 1;
                 }
             }
-            libc_println!("{} {} {}",values[0],values[1],values[2]);
+            libc_println!("{} : {} {} {}",frame,values[0],values[1],values[2]);
             scanKeys();
             if (keysDown() & KEY_START) > 0
             {
@@ -30,6 +30,7 @@ fn main(_argc: isize, _argv: *const *const u8) -> isize
                 swiWaitForVBlank();
                 break;
             }
+            frame += 1;
             swiWaitForVBlank();
         }
     }
