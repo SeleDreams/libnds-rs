b2aa2dcc8a28c1f699553a7bbf5baeae9ab31135
IoI_xD <alphaproject217@gmail.com>
2023-03-16 23:00:20 -0700

    forgot to remove arm9_registers from the lib.rs

--- libnds-sys/src/lib.rs
+++ libnds-sys/src/lib.rs
@@ -9,7 +9,6 @@ pub mod arm7_registers;
 
 #[allow(non_upper_case_globals,non_camel_case_types,non_snake_case,improper_ctypes,dead_code)]
 pub mod arm9_bindings;
-pub mod arm9_registers;
 
 pub mod video_registers;
 pub mod background_registers;
